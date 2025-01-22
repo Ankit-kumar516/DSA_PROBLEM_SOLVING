43869b1fb01cad0057cdc53effad72585758d6e4
Ankit-kumar516 <atulayaankit516@gmail.com>
2025-01-22 23:31:19 +0530

    access_code_equality

---
+++ Access Code Equality.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    string s, str;
+    str="WECNITK";
+    cin>>s;
+    if(s==str)
+    {
+        cout<<"Welcome to Web Club!";
+    }
+    else
+    {
+        cout<<"Access denied";
+    }
+}
